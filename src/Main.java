aacc0e8a14f5051ff4546a11a1ac5b8b17d22b62
nosabr <sabyrzhan.syrym@gmail.com>
2023-11-08 20:10:20 +0500

    12

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Game{
 public class Main {
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);// scanner
+        Scanner in = new Scanner(System.in);// scanner created
 
         String secret_word = "hello";
         int secret_word_len = secret_word.length(); // len of the word
