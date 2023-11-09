fed631a962120f17a24aaaf1e9e0719f47cc51f2
nosabr <syrym.sabyrzhan@gmail.com>
2023-11-09 16:02:59 +0600

    5

--- src/Game.java
+++ src/Game.java
@@ -13,22 +13,57 @@ public class Game {
             menu();
         }
     }
+    public static boolean check(char letter, String secret_word){
+        if(secret_word.indexOf(letter) == -1){
+            System.out.println("You missed!");
+            return false;
+        } else {
+            System.out.println("You found it!");
+            return true;
+        }
+    }
+    public static char input(String star_word){
+        System.out.print("Enter your letter: ");
+        char letter = scanner.next().charAt(0);
+        if (star_word.indexOf(letter) != -1) {
+            System.out.println("You already found that letter, try again!");
+            letter = input(star_word);
+        }
+        return letter;
+    }
+    public static String right(char letter, String star_word, String secret_word){
+        StringBuilder updated_star_word = new StringBuilder(star_word);
+        for(int i = 0; i < secret_word.length(); i++){
+            if(secret_word.charAt(i)== letter){
+                updated_star_word.setCharAt(i, letter);
+            }
+        }
+        return updated_star_word.toString();
+    }
+
+    public static void wrong(){
+
+    }
+
     public static void startgame (String secret_word){
         int secret_word_len = secret_word.length(); // len of the word
         String star_word = "*".repeat(secret_word_len); // word with ***
-        int attemts = 4;
-        while (attemts != 0){
+        int attempts = 4;
+        while (attempts != 0){
             if (star_word.equals(secret_word)){
                 endgame(true);
             }
             System.out.println("Secret word: " + star_word);
-            System.out.println("You got " + attemts + "attemts.");
+            System.out.println("You got " + attempts + " attempts.");
             System.out.print("Enter your letter: ");
-            char letter = scanner.next().charAt(0);
-            /*
-            if (secret_word.contains(letter)){
-
-            }*/
+            char letter = input(star_word);
+            if (check(letter, secret_word)){
+                star_word = right(letter,star_word, secret_word);
+                // zapusk right string function
+            } else {
+                attempts -= 1;
+                // wrong
+            }
         }
         endgame(false);
 
--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);// scanner created
-
-        String secret_word = "hello";
-        int secret_word_len = secret_word.length(); // len of the word
-        String star_word = "*".repeat(secret_word_len); // word with ****
-
-
-        System.out.print("[n]ew game or [e]xit?: ");
-        String conf = in.next();
-        if (Objects.equals(conf, "e")) {
-            return; // ??? remove and use functions!
-        }
-
-        int attempts = 4;
-        while (attempts != 0){
-            System.out.println("You have " + attempts + "left.");
-            System.out.print("Enter letter: ");
-            char letter = in.next().charAt(0); // input letter
-
-        }
+        Game.menu();
     }
 }
 
